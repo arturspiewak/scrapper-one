f93224b5b25eb89882f7bcf2a443f5fd00140083
kpietkaa <kpietkaa@gmail.com>
2016-12-27 17:43:34 +0100

    Add new, create actions

--- app/controllers/saxes_controller.rb
+++ app/controllers/saxes_controller.rb
@@ -3,4 +3,36 @@ class SaxesController < ApplicationController
   def index
   end
 
+  def new
+    @address = Sax.new
+  end
+
+  def create
+    @address = Sax.new(sax_params)
+    data = scrape params[:sax][:address]
+
+    if data[:error].nil?
+      @address.title = data[:title]
+      @address.price = data[:price]
+      @address.timestamp = data[:timestamp]
+
+      if @address.save
+        redirect_to root_path
+      else
+        render 'new'
+      end
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def sax_params
+    params.require(:sax).permit(:title, :price, :timestamp)
+  end
+
+  def scrape(url)
+    s = Scraping.new
+    s.scrape(url).to_h
+  end
 end
